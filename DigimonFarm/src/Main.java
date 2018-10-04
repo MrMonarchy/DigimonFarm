1823bf90dfc1710dd99a53b926812115ba025a22
Eli <Eli@10.100.148.132>
2018-10-04 09:20:25 -0400

    Initial Commit

---
+++ DigimonFarm/src/Main.java
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+//Elijah Hernandez
+//Program where you raise and train your own Digimon
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome Digital Trainer!");
+		
+		System.out.println("Let's begin!");
+
+		System.out.println("Since you're a new trainer, I think this Digimon would be more your speed.");
+		
+		System.out.println("This is Koromon! He is an In-Training Digimon, but don't let his size fool you. This small pink creature has a mean bite and a fiery temper.");
+		
+	
+		String name = (String)JOptionPane.showInputDialog(null,
+				
+				"What will you call your Koromon?",
+				"DigiFarm",
+				
+				2,
+				null,
+				null,
+				"Enter your name here");
+		
+		System.out.println(name + ", what a nice name!");
+		
+		System.out.println("Now that " + name + " has a name, it's time to head to the farm!");
+		
+	
+		
+				
+				
+	
+	
+	
+}
+
+	
+	}
+	
+	
+
+
